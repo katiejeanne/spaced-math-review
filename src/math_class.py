381a45af12303db885a8c8c11add14ca5837f805
Katie Wessman <kjeanne@gmail.com>
2024-02-20 13:21:10 +0800

    Improve date handling

--- src/math_class.py
+++ src/math_class.py
@@ -3,37 +3,48 @@ import sqlite3
 
 
 class Exercise:
-    def __init__(self, exercise_id="000", name="Unnamed exercise", web_link="0", current_stage="0", last_review_date="0", due_date="0") -> None:
+    def __init__(self, exercise_id, name, web_link, class_id=0, current_stage=0, last_review_date=None, due_date=None) -> None:
         self.exercise_id = exercise_id
         self.name = name
         self.web_link = web_link
+        self.class_id = class_id
         self.current_stage = current_stage
-        self.last_review_date = self._return_date_or_zero(last_review_date)
-        self.due_date = self._return_date_or_zero(due_date)
+        self.last_review_date = self._return_date_or_none(last_review_date)
+        self.due_date = self._return_date_or_none(due_date)
         
     
     def print_exercise(self):
-        print(self.exercise_id, self.name, self.current_stage, self.last_review_date, self.due_date)
+        print(self.exercise_id, self.name, self.web_link, self.class_id, self.current_stage, self._return_datestring_or_nastring(self.last_review_date), self._return_datestring_or_nastring(self.due_date))
 
-    def _return_date_or_zero(self, date_string):
-        try: 
-            if date_string == "0":
-                return "0"
-            else:
-                return self._date_string_to_object(date_string)
-        except ValueError as e:
-            raise ValueError(f"Invalid date format for {date_string}, expected format: YYYY-MM-DD") from e
+    def _return_date_or_none(self, date_string):
+        if not date_string:
+            return None
+        else:
+            return self._date_string_to_object(date_string)
+        
 
     def _date_string_to_object(self, date_string):
-        return datetime.datetime.strptime(date_string, "%Y-%m-%d")
+        try: 
+            return datetime.datetime.strptime(date_string, "%Y-%m-%d")
+        except ValueError as e:
+            raise ValueError(f"Invalid date format for {date_string}, expected format: YYYY-MM-DD") from e
+    
+    def _return_datestring_or_nastring(self, date):
+        # Converts date object to string for printing. If no date value is assigned "n/a" is printed.
+        if not date:
+            return "n/a"
+        else:
+            return date.strftime("%Y-%m-%d")
 
 class MathClass:
 
-    def __init__(self) -> None:
+    def __init__(self, class_id=0, class_name="Default") -> None:
         self.exercises = []
+        self.class_id = class_id
+        self.class_name = class_name
 
-    def add_exercise(self, exercise_id, name, web_link, stage="0", last_review_date="0", due_date="0"):
-        new_exercise = Exercise(exercise_id, name, web_link, stage, last_review_date, due_date)
+    def add_exercise(self, exercise_id, name, web_link):
+        new_exercise = Exercise(exercise_id, name, web_link, self.class_id)
         self.exercises.append(new_exercise)
 
     def print_all_exercises(self):
@@ -50,6 +61,7 @@ class MathClass:
                                 id TEXT PRIMARY KEY,
                                 name TEXT,
                                 web_link TEXT,
+                                class_id INTEGER,
                                 current_stage INTEGER,
                                 last_review_date TEXT,
                                 due_date TEXT)''')
@@ -57,9 +69,9 @@ class MathClass:
             # Insert or update exercises in the database
             for exercise in self.exercises:
                 cursor.execute('''INSERT OR REPLACE INTO exercises 
-                                (id, name, web_link, current_stage, last_review_date, due_date) 
-                                VALUES (?, ?, ?, ?, ?, ?)''', 
-                            (exercise.exercise_id, exercise.name, exercise.web_link, exercise.current_stage, 
+                                (id, name, web_link, class_id, current_stage, last_review_date, due_date) 
+                                VALUES (?, ?, ?, ?, ?, ?, ?)''', 
+                            (exercise.exercise_id, exercise.name, exercise.web_link, exercise.class_id, exercise.current_stage, 
                                 exercise.last_review_date, exercise.due_date))
 
             conn.commit()
--- tests/test_math_class.py
+++ tests/test_math_class.py
@@ -5,27 +5,16 @@ import sqlite3
 from src.math_class import MathClass, Exercise
 
 # Define the parameter sets
-@pytest.mark.parametrize("exercise_id, name, web_link, stage, last_review_date, due_date", [
-    ("ex1", "Addition Basics", "https://www.example.com/addition", 1, "2023-02-20", "2023-02-27"),
-    ("ex2", "Subtraction Basics", "https://www.example.com/subtraction", 2, "2023-03-01", "2023-03-05"),
-    ("000.000.000", "This is a very long title. It's all about how math exercies are so fascinating. Greebo loves math. Kalyn loves math. We all love math.", "https://www.khanacademy.org/math/pre-algebra", 20, "0", "0")
+@pytest.mark.parametrize("exercise_id, name, web_link", [
+    ("ex1", "Addition Basics", "https://www.example.com/addition"),
+    ("ex2", "Subtraction Basics", "https://www.example.com/subtraction"),
+    ("000.000.000", "This is a very long title. It's all about how math exercies are so fascinating. Greebo loves math. Kalyn loves math. We all love math.", "https://www.khanacademy.org/math/pre-algebra")
 ])
-def test_add_exercise(exercise_id, name, web_link, stage, last_review_date, due_date):
-    math_class = MathClass()
-
-    # Convert string dates to datetime objects for comparison
-    if last_review_date != "0":
-        last_review_date_object = datetime.datetime.strptime(last_review_date, "%Y-%m-%d")
-    else:
-        last_review_date_object = last_review_date
-
-    if due_date != "0":
-        due_date_object = datetime.datetime.strptime(due_date, "%Y-%m-%d")
-    else:
-        due_date_object = due_date
+def test_add_exercise(exercise_id, name, web_link):
+    math_class = MathClass(0, "Test math class")
 
     # Add an exercise to the math_class
-    math_class.add_exercise(exercise_id, name, web_link, stage, last_review_date, due_date)
+    math_class.add_exercise(exercise_id, name, web_link)
 
     # Check if the exercise was added correctly
     added_exercise = math_class.exercises[-1]  # Access the last added exercise
@@ -34,44 +23,26 @@ def test_add_exercise(exercise_id, name, web_link, stage, last_review_date, due_
     assert added_exercise.exercise_id == exercise_id
     assert added_exercise.name == name
     assert added_exercise.web_link == web_link
-    assert added_exercise.current_stage == stage
-    assert added_exercise.last_review_date == last_review_date_object
-    assert added_exercise.due_date == due_date_object
+    assert added_exercise.class_id == 0
 
-
-
-def test_add_exercise_with_invalid_date_raises_value_error():
-    
-    # Create an instance of MathClass
-    math_class = MathClass()
-
-    # Define the exercise details with an invalid date
-    exercise_id = "ex1"
-    name = "Addition Basics"
-    web_link = "https://www.khanacademy.org/math/pre-algebra"
-    stage = 1
-    invalid_last_review_date = "invalid-date"
-    due_date = "2023-02-27"
-
-    # Use pytest.raises to expect a ValueError
-    with pytest.raises(ValueError) as excinfo:
-        math_class.add_exercise(exercise_id, name, web_link, stage, invalid_last_review_date, due_date)
 
 @pytest.fixture
 def setup_math_class_with_exercises():
-    math_class = MathClass()
+    math_class = MathClass(0,"Test math class")
     # Create mock exercises
-    exercise1 = Exercise("001", "Exercise 1", "http://example.com/1", 1, "2023-01-01", "2023-01-02")
-    exercise2 = Exercise("002", "Exercise 2", "http://example.com/2", 2, "2023-01-03", "2023-01-04")
+    exercise1 = Exercise("001", "Exercise 1", "http://example.com/1", 0, 1, "2023-01-01", "2023-01-02")
+    exercise2 = Exercise("002", "Exercise 2", "http://example.com/2", 0, 2, "2023-01-03", "2023-01-04")
+    exercise3 = Exercise("123.123.123", "Exercise 3", "www.math.org", 1, 6, None, None)
     # Add mock exercises to MathClass instance
-    math_class.exercises.extend([exercise1, exercise2])
+    math_class.exercises.extend([exercise1, exercise2, exercise3])
     return math_class
 
 def test_save_to_db(setup_math_class_with_exercises, tmp_path):
-    db_path = tmp_path / "test_database.db"  # Use pytest's tmp_path fixture for a temporary database file
+    db_path = tmp_path / "test2_database.db"  # Use pytest's tmp_path fixture for a temporary database file
     math_class = setup_math_class_with_exercises
 
     # Invoke the method to test
+    math_class.print_all_exercises()
     math_class.save_to_db(db_path=db_path.as_posix())
 
     # Connect to the temporary database and verify the inserted data
@@ -81,7 +52,7 @@ def test_save_to_db(setup_math_class_with_exercises, tmp_path):
     rows = cursor.fetchall()
 
     # Verify the number of inserted rows and some specific data
-    assert len(rows) == 2  # Assuming you added 2 exercises
+    assert len(rows) == 3  # Assuming you added 2 exercises
     assert rows[0][0] == "001"  # Verify the first exercise's ID
     assert rows[1][1] == "Exercise 2"  # Verify the second exercise's name
 
